279650dd16906083c389bb57d5897f73fc6db925
Paul <CWR.Amit.Paul@int.asurion.com>
2018-07-26 15:59:54 -0500

    added port

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 const express =require('express');
 const app =express();
 
+app.set('PORT', process.env.PORT || 3000);
+
 app.get('/',(req,res,next)=>{
     res.send("hello from node app");
 });
@@ -10,7 +12,6 @@ app.get('/',(req,res,next)=>{
 app.get('/dashbord',(req,res,next)=>{
     res.send("hello from node app dashord");
 });
-
-app.listen(3000,()=>{
+app.listen(app.get('PORT'),()=>{
     console.log("server running on port 3000");
 });
